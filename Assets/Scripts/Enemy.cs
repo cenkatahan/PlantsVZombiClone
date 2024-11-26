5574859a88d7d6252bd62bd42eb11d5d11a10fe5
Cenk Atahan Özbek <49042414+cenkatahan@users.noreply.github.com>
2024-11-26 22:47:57 +0300

    update sprites for map and gamebjects

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,9 +6,13 @@ public class Enemy : MonoBehaviour
     [SerializeField] private new Rigidbody2D rigidbody2D;
     [SerializeField] private float moveSpeed = 5f;
 
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+    }
+
     void Update()
     {
         rigidbody2D.linearVelocity = new Vector2(-moveSpeed, rigidbody2D.linearVelocity.y);
     }
-    
 }
